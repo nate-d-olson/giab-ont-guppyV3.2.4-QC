cb87c9aebea30919a4e019cfc3afea0b7f7b0c1d
nate-d-olson <nolson@nist.gov>
2019-11-20 10:36:47 -0500

    added bam stat sync and renamed file

--- scripts/sync_pipe_output_files.sh
+++ scripts/sync_pipe_output_files.sh
@@ -3,3 +3,9 @@ rsync -rvm --delete \
 	--include="*/" --include="*sequencing_summary.txt" --exclude="*" \
 	sherlock:/scratch/groups/msalit/nanopore/processing/guppy-3.2.4-snakemake-pipe/ \
 	data/guppy_V3.2.4/sequencing_summary/
+
+## Sync BAM stats files
+rsync -rvm --delete \
+	--include="*/" --include="*.bam.stats.tsv.gz" --exclude="*" \
+	sherlock:/scratch/groups/msalit/nanopore/processing/guppy-3.2.4-snakemake-pipe/ \
+	data/guppy_V3.2.4/bam_stats/
